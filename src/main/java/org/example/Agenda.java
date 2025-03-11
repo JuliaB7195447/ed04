1d8917155056b3c25e0d1c34c57449317cd4f805
Julia Bosianek <frontend.julia@gmail.com>
2025-03-11 02:33:53 +0100

    Se inserta los comentarios JavaDoc en las clases Agenda y Contacto.

--- src/main/java/org/example/Agenda.java
+++ src/main/java/org/example/Agenda.java
@@ -4,13 +4,31 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * Clase que representa una agenda de contactos.
+ * Contiene metodos para añadir, eliminar, y modificar los contactos.
+ *
+ * @author CIFP Carlos III
+ * @version 1.0 11/03/2025
+ * @see Contacto
+ */
 public class Agenda {
     private List<Contacto> contacts; // Lista de Contacto
 
+    /**
+     * Constructor de la clase Agenda.
+     * Inicializa nueva lista de contactos.
+     */
     public Agenda() {
         this.contacts = new ArrayList<>();
     }
 
+    /**
+     * Agrega un nuevo contacto o añade un nuevo numero de telefono a contacto ya existente.
+     *
+     * @param name Nombre del contacto.
+     * @param phone Numero de telefono para añadir
+     */
     public void addContact(String name, String phone) {
         boolean exists = false;
         for (Contacto c : contacts) {
@@ -21,12 +39,17 @@ public class Agenda {
             }
         }
 
-        if (!exists) {
+        if (!exists) {      // si el contacto no existe
             Contacto newContact = new Contacto(name, phone);
             contacts.add(newContact);
         }
     }
 
+    /**
+     * Elimina un contacto existente de la agenda.
+     *
+     * @param name Nombre del contacto para eliminar.
+     */
     public void removeContact(String name) {
         Iterator<Contacto> it = contacts.iterator();
 
@@ -39,6 +62,13 @@ public class Agenda {
         }
     }
 
+    /**
+     * Modifica el numero de telefono de un contacto existente.
+     *
+     * @param name Nombre del contacto.
+     * @param oldPhone Numero de telefono para modificar.
+     * @param newPhone Nuevo numero de telefono a reemplazar.
+     */
     public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
         for (Contacto c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
@@ -53,6 +83,12 @@ public class Agenda {
         }
     }
 
+    /**
+     * Obtiene la lista de contactos guardados en la agenda.
+     *
+     * @return Lista de contactos.
+     * @see Contacto
+     */
     public List<Contacto> getContacts() {
         return this.contacts;
     }
--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -3,20 +3,43 @@ package org.example;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Clase que representa un contacto en la agenda, compuesto por un nombre y una lista de telefonos asignados.
+ *
+ * @author CIFP Carlos III
+ * @version 1.0 11/03/2025
+ */
 class Contacto {
-    private String name;
-    private List<String> phones;
+    private String name; // Nombre del contacto
+    private List<String> phones; // Lista de telefonos asignados.
 
+    /**
+     * Constructor para crear un nuevo contacto.
+     *
+     * @param name Nombre del contacto.
+     * @param phone Numero de telefono del contacto.
+     */
     public Contacto(String name, String phone) {
         this.name = name;
         this.phones = new ArrayList<>();
         this.phones.add(phone);
     }
 
+    /**
+     * Obtiene el nombre del contacto.
+     *
+     * @return Devuelve nombre del contacto.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Obtiene la lista de numeros de telefono del contacto.
+     *
+     * @return Lista de numeros de telefono.
+     * @see List
+     */
     public List<String> getPhones() {
         return this.phones;
     }
